41027dfbecc054fa7de244ac267a8ed5c4648268
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-08 11:06:15 -1000

    passed all tests

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,13 +1,41 @@
 exports.largestPrimeFactor = function(n){
   var primeNumber = 0;
   // do your work here
-  var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
 
-  for(let i = 0; i < primeNumbers.length; i++) {
-    if(n % primeNumbers[i] === 0) {
-      primeNumber = primeNumbers[i];
+  function primeFactorization(x) {
+    let pN = x;
+    let i = 2;
+    let sqrt = Math.floor(Math.sqrt(x));
+
+    while(i <= sqrt)
+    {
+      i++;
+      if(i === 2) {
+        return i;
+      }
+      if (x % i === 0 && isPrime(i)) {
+        pN = i;
+      }
     }
+    return pN;
   }
+
+  function isPrime(x) {
+
+    if(x < 2) {
+      return false;
+    } else {
+      for(let i = 2; i < x; i++) {
+        if (x % i === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+  }
+
+  primeNumber = primeFactorization(n);
 
   return primeNumber;
 };
