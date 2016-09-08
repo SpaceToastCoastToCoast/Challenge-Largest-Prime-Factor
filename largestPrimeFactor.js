54daa00036f36f138c66a3364ff57180dd124097
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-08 09:21:52 -1000

    passed first test: using an array of prime numbers and iterating through

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,13 @@
 exports.largestPrimeFactor = function(n){
   var primeNumber = 0;
   // do your work here
+  var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+
+  for(let i = 0; i < primeNumbers.length; i++) {
+    if(n % primeNumbers[i] === 0) {
+      primeNumber = primeNumbers[i];
+    }
+  }
 
   return primeNumber;
 };
